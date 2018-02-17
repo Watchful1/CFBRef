6324e858eb3b0cc0038def2a58cb9ed54b5a51f6
Watchful1 <watchful@watchful.gr>
2018-02-17 13:47:34 -0800

    Increase log size

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ if not os.path.exists(globals.LOG_FOLDER_NAME):
 	os.makedirs(globals.LOG_FOLDER_NAME)
 LOG_FILENAME = globals.LOG_FOLDER_NAME+"/"+"bot.log"
 LOG_FILE_BACKUPCOUNT = 5
-LOG_FILE_MAXSIZE = 1024 * 256
+LOG_FILE_MAXSIZE = 1024 * 256 * 16
 
 log = logging.getLogger("bot")
 log.setLevel(LOG_LEVEL)
