79a988f767753e3930e508fa3066b426da5499f6
Watchful1 <watchful@watchful.gr>
2018-03-17 18:12:53 -0700

    Handle the api going down and the message stream being interrupted

--- src/main.py
+++ src/main.py
@@ -76,78 +76,89 @@ database.init()
 
 wiki.loadPages()
 
-for message in reddit.getMessageStream():
-	startTime = time.perf_counter()
-	log.debug("Processing message")
-	wiki.loadPages()
-
+while True:
 	try:
-		messages.processMessage(message)
+		for message in reddit.getMessageStream():
+			startTime = time.perf_counter()
+			log.debug("Processing message")
+			wiki.loadPages()
+
+			try:
+				messages.processMessage(message)
+			except Exception as err:
+				log.warning("Error in main loop")
+				log.warning(traceback.format_exc())
+				if globals.gameId is not None:
+					log.debug("Setting game {} as errored".format(globals.gameId))
+					database.setGameErrored(globals.gameId)
+					ownerMessage = "[Game]({}) errored. Click [here]({}) to clear."\
+						.format("{}{}".format(globals.SUBREDDIT_LINK, globals.logGameId[1:-1]),
+					            utils.buildMessageLink(
+			                        globals.ACCOUNT_NAME,
+			                        "Kick game",
+			                        "kick {}".format(globals.gameId)
+			                    ))
+				else:
+					ownerMessage = "Unable to process message from /u/{}, skipping".format(str(message.author))
+
+				try:
+					reddit.sendMessage(globals.OWNER, "NCFAA game errored", ownerMessage)
+					message.mark_read()
+				except Exception as err2:
+					log.warning("Error sending error message")
+					log.warning(traceback.format_exc())
+
+			for threadId in database.getGamesPastPlayclock():
+				log.debug("Game past playclock: {}".format(threadId))
+				game = utils.getGameByThread(threadId)
+				game[game['waitingOn']]['playclockPenalties'] += 1
+				penaltyMessage = "{} has not sent their number in over 24 hours, playclock penalty. This is their {} penalty.".format(
+					utils.getCoachString(game, game['waitingOn']), utils.getNthWord(game[game['waitingOn']]['playclockPenalties']))
+				if game[game['waitingOn']]['playclockPenalties'] >= 3:
+					log.debug("3 penalties, game over")
+					game['status']['quarterType'] = 'end'
+					game['waitingAction'] = 'end'
+					resultMessage = "They forfeit the game. {} has won!".format(utils.flair(game[utils.reverseHomeAway(game['waitingOn'])]))
+
+				elif game['waitingOn'] == game['status']['possession']:
+					log.debug("Waiting on offense, turnover")
+					if utils.isGameOvertime(game):
+						resultMessage = state.overtimeTurnover(game)
+						if game['waitingAction'] != 'end':
+							utils.sendDefensiveNumberMessage(game)
+					else:
+						state.turnover(game)
+						utils.sendDefensiveNumberMessage(game)
+						resultMessage = "Turnover, {} has the ball.".format(utils.flair(game[game['waitingOn']]))
+
+				else:
+					log.debug("Waiting on defense, touchdown")
+					if utils.isGameOvertime(game):
+						state.forceTouchdown(game, game['status']['possession'])
+						resultMessage = state.overtimeTurnover(game)
+						if game['waitingAction'] != 'end':
+							utils.sendDefensiveNumberMessage(game)
+					else:
+						state.forceTouchdown(game, game['status']['possession'])
+						state.setStateTouchback(game, utils.reverseHomeAway(game['status']['possession']))
+						utils.sendDefensiveNumberMessage(game)
+						resultMessage = "Automatic 7 point touchdown, {} has the ball.".format(utils.flair(game[game['waitingOn']]))
+
+				utils.sendGameComment(game, "{}\n\n{}".format(penaltyMessage, resultMessage))
+				database.setGamePlayed(game['dataID'])
+				utils.updateGameThread(game)
+
+			log.debug("Message processed after: %d", int(time.perf_counter() - startTime))
+			utils.clearLogGameID()
+			if once:
+				database.close()
+				break
+
 	except Exception as err:
-		log.warning("Error in main loop")
+		log.warning("Hit an error in main loop")
 		log.warning(traceback.format_exc())
-		if globals.gameId is not None:
-			log.debug("Setting game {} as errored".format(globals.gameId))
-			database.setGameErrored(globals.gameId)
-			ownerMessage = "[Game]({}) errored. Click [here]({}) to clear."\
-				.format("{}{}".format(globals.SUBREDDIT_LINK, globals.logGameId[1:-1]),
-			            utils.buildMessageLink(
-                            globals.ACCOUNT_NAME,
-                            "Kick game",
-                            "kick {}".format(globals.gameId)
-                        ))
-		else:
-			ownerMessage = "Unable to process message from /u/{}, skipping".format(str(message.author))
 
-		try:
-			reddit.sendMessage(globals.OWNER, "NCFAA game errored", ownerMessage)
-			message.mark_read()
-		except Exception as err2:
-			log.warning("Error sending error message")
-			log.warning(traceback.format_exc())
-
-	for threadId in database.getGamesPastPlayclock():
-		log.debug("Game past playclock: {}".format(threadId))
-		game = utils.getGameByThread(threadId)
-		game[game['waitingOn']]['playclockPenalties'] += 1
-		penaltyMessage = "{} has not sent their number in over 24 hours, playclock penalty. This is their {} penalty.".format(
-			utils.getCoachString(game, game['waitingOn']), utils.getNthWord(game[game['waitingOn']]['playclockPenalties']))
-		if game[game['waitingOn']]['playclockPenalties'] >= 3:
-			log.debug("3 penalties, game over")
-			game['status']['quarterType'] = 'end'
-			game['waitingAction'] = 'end'
-			resultMessage = "They forfeit the game. {} has won!".format(utils.flair(game[utils.reverseHomeAway(game['waitingOn'])]))
-
-		elif game['waitingOn'] == game['status']['possession']:
-			log.debug("Waiting on offense, turnover")
-			if utils.isGameOvertime(game):
-				resultMessage = state.overtimeTurnover(game)
-				if game['waitingAction'] != 'end':
-					utils.sendDefensiveNumberMessage(game)
-			else:
-				state.turnover(game)
-				utils.sendDefensiveNumberMessage(game)
-				resultMessage = "Turnover, {} has the ball.".format(utils.flair(game[game['waitingOn']]))
-
-		else:
-			log.debug("Waiting on defense, touchdown")
-			if utils.isGameOvertime(game):
-				state.forceTouchdown(game, game['status']['possession'])
-				resultMessage = state.overtimeTurnover(game)
-				if game['waitingAction'] != 'end':
-					utils.sendDefensiveNumberMessage(game)
-			else:
-				state.forceTouchdown(game, game['status']['possession'])
-				state.setStateTouchback(game, utils.reverseHomeAway(game['status']['possession']))
-				utils.sendDefensiveNumberMessage(game)
-				resultMessage = "Automatic 7 point touchdown, {} has the ball.".format(utils.flair(game[game['waitingOn']]))
-
-		utils.sendGameComment(game, "{}\n\n{}".format(penaltyMessage, resultMessage))
-		database.setGamePlayed(game['dataID'])
-		utils.updateGameThread(game)
-
-	log.debug("Message processed after: %d", int(time.perf_counter() - startTime))
-	utils.clearLogGameID()
 	if once:
-		database.close()
 		break
+
+	time.sleep(5 * 60)
