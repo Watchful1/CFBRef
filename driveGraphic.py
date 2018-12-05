d6330167c7b5a73f9ac25903acb7434d07406748
Nate Harris <nwithan8@users.noreply.github.com>
2018-12-04 23:57:58 -0500

    Create driveGraphic.py
    Possible graphics creation

---
+++ driveGraphic.py
@@ -0,0 +1,65 @@
+from PIL import Image, ImageColor, ImageDraw
+from pathlib import Path
+
+height = 53
+width = 120
+adj = width/120
+
+run_color = "red"
+pass_color = "blue"
+
+driveEnders = [Result.TURNOVER, Result.TOUCHDOWN, Result.TURNOVER_TOUCHDOWN, Result.FIELD_GOAL, Result.PUNT]
+
+def makeNewField(team_with_possession,list_of_teams,location): #later, implement unique ids for filenames to correspond with games
+    field = Image.new(mode='RGB', size=(width,height), color="green")
+    x_start = 10*adj
+    x_end = 110*adj
+    x_step = 5*adj
+    for x in range(0,x_start):
+        line = ((x,0),(x,field.height))
+        draw.line(line,fill="lightgrey")
+    for x in range(x_end+1,field.width):
+        line = ((x,0),(x,field.height))
+        draw.line(line,fill="lightgrey")
+    for x in range(x_start,x_end+1,x_step):
+        line = ((x, 0),(x,field.height))
+        if x == 10*adj or x == 110*adj:
+            draw.line(line,fill="black")
+        else:
+            draw.line(line,fill="grey")
+    #only time file should not exist is when drive first starts. Adds initial LoS here.
+    if team_with_possession == list_of_teams[0]: #team1 goes left to right, team2 goes right to left
+         line = (((position+10)*adj,0),((position+10)*adj,field.height))
+    else:
+         line = ((((100-position)+10)*adj,0),(((100-position)+10)*adj,field.height))
+    draw.line(line,fill="white")
+    return field
+    
+def addToField(list_of_teams,List_of_Plays):
+    #currently, keeping track of play results
+    #places starting line of scrimmage for drive, and then lines for each play
+    field = ""
+    if Path("field_ID.png").is_file():
+        field = Image.open('field_ID.png')
+    else:
+        field = makeNewField(team_with_possession,list_of_teams,location) #need starting position (LoS from kickoff/punt/pick)
+        field.save('field_ID.png',"PNG")
+    line_y_position = 5
+    draw = ImageDraw.Draw(field)
+    line = ((0,0),(0,0))
+    for play in List_of_Plays:
+        if play.result in driveEnders: #drive is over, need to handle FG, TOUCHDOWNS, and KICKOFFS eventually. Currently showing only non-scoring runs and passes
+            field.save('field_ID.png',"PNG")
+            break
+        else:
+            if line_y_position > field.height:
+                line_y_position = 5
+            if play.posHome == True: #home team has it, going left to right
+                line = ((play.yards+10)*adj,line_y_position),((play.location+10)*adj,line_y_position))
+            else: #away team has it, going right to left
+                line = ((100-play.yards+10)*adj,line_y_position),((100-play.location+10)*adj,line_y_position))
+            if play.play == RUN:
+                draw.line(line,color=run_color)
+            if play.play == PASS:
+                draw.line(line,color=pass_color)
+            field.save('field_ID.png',"PNG")
